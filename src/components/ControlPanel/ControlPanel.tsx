1ada1a0484c031b6802245eee2a8e58565b4c682
katsu <sdjgsinhzoghaodg@yandex.ru>
2025-11-17 12:30:10 +0300

    refactored control panel component to class version

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,7 +1,24 @@
 import ControlPanelLayout from './ControlPanelLayout';
-import { useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
+import { Component } from 'react';
 
-export function ControlPanel() {
+export class CControlPanel extends Component<any, any> {
+	constructor(props: any) {
+		super(props);
+	}
+	
+	clearField = () => {
+		this.props.dispatch({ type: 'RESTART_GAME' });
+	};
+	
+	render() {
+		return <ControlPanelLayout onClick={this.clearField}></ControlPanelLayout>;
+	}
+}
+
+export const ControlPanel = connect(null)(CControlPanel);
+
+export function FControlPanel() {
 	const dispatch = useDispatch();
 
 	const clearField = () => {
