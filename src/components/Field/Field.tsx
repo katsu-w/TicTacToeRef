d71af129af1e0d49b157ee68d439f46a8c7428fb
katsu <sdjgsinhzoghaodg@yandex.ru>
2025-11-17 12:24:34 +0300

    refactored field component to class version

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,13 +1,45 @@
 import FieldLayout from './FieldLayout';
-import type { currentPlayerType, fieldType } from '../../types/types.ts';
-import { useDispatch, useSelector } from 'react-redux';
+import type {
+	currentPlayerType,
+	fieldType,
+	IStore,
+} from '../../types/types.ts';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import {
 	currentPlayerSelector,
 	fieldSelector,
 	isGameEndedSelector,
 } from '../../selectors/selectors.ts';
+import { Component } from 'react';
 
-export function Field() {
+export class CField extends Component<any, any> {
+	constructor(props: any) {
+		super(props);
+	}
+	
+	onClick = (index: number) => {
+		if (!this.props.isGameEndedStore && !this.props.fieldStore[index]) {
+			let newField = [...this.props.fieldStore];
+			newField[index] = this.props.currentPlayerStore;
+			this.props.dispatch({ type: 'SET_MARK', payload: { fieldStore: newField } });
+		}
+	}
+	
+	render() {
+		return <FieldLayout onClick={this.onClick} field={this.props.fieldStore}></FieldLayout>;
+	}
+}
+
+const mapStateToProps = (state: IStore): IStore => ({
+	fieldStore: state.fieldStore,
+	isGameEndedStore: state.isGameEndedStore,
+	isDrawStore: state.isDrawStore,
+	currentPlayerStore: state.currentPlayerStore,
+});
+
+export const Field = connect(mapStateToProps)(CField);
+
+export function FField() {
 	const currentPlayer: currentPlayerType = useSelector(currentPlayerSelector);
 	const isGameEnded: boolean = useSelector(isGameEndedSelector);
 	const field: fieldType = useSelector(fieldSelector);
