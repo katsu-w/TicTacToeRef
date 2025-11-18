be4a987fabbecd284d27dc50eef0850f709da9fd
katsu <sdjgsinhzoghaodg@yandex.ru>
2025-11-18 13:55:42 +0300

    refactored control panel layout component to class version

--- src/components/ControlPanel/ControlPanelLayout/ControlPanelLayout.tsx
+++ src/components/ControlPanel/ControlPanelLayout/ControlPanelLayout.tsx
@@ -1,10 +1,25 @@
 import './ControlPanelLayout.scss';
+import { Component } from 'react';
 
 interface IControlPanelLayoutProps {
 	onClick: () => void;
 }
 
-export function ControlPanelLayout({ onClick }: IControlPanelLayoutProps) {
+export class ControlPanelLayout extends Component<any, any> {
+	constructor(props: IControlPanelLayoutProps) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<button onClick={() => this.props.onClick()} className="retry-btn">
+				Начать заново
+			</button>
+		);
+	}
+}
+
+export function FControlPanelLayout({ onClick }: IControlPanelLayoutProps) {
 	return (
 		<button onClick={() => onClick()} className="retry-btn">
 			Начать заново
