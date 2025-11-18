16a87b1a3ec105ae99226e19e41f32b389605196
katsu <sdjgsinhzoghaodg@yandex.ru>
2025-11-18 13:53:39 +0300

    refactored field layout component to class version

--- src/components/Field/FieldLayout/FieldLayout.tsx
+++ src/components/Field/FieldLayout/FieldLayout.tsx
@@ -1,11 +1,33 @@
 import './FieldLayout.scss';
+import { Component } from 'react';
 
 interface IFieldLayoutProps {
 	field: string[];
 	onClick: (index: number) => void;
 }
 
-export function FieldLayout({ field, onClick }: IFieldLayoutProps) {
+export class FieldLayout extends Component<any, any> {
+	constructor(props: IFieldLayoutProps) {
+		super(props);
+	}
+	render() {
+		return (
+			<section className="playground">
+				{this.props.field.map((cell: string, index: number) => (
+					<button
+						key={index}
+						className="playground__cell"
+						onClick={() => this.props.onClick(index)}
+					>
+						{cell}
+					</button>
+				))}
+			</section>
+		);
+	}
+}
+
+export function FFieldLayout({ field, onClick }: IFieldLayoutProps) {
 	return (
 		<section className="playground">
 			{field.map((cell, index) => (
